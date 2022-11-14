7345fe68b7f54a625c79bde6f3923c706dcc89ce
Martin Estrada <mestra18@asu.edu>
2022-11-13 21:22:03 -0700

    first commit

---
+++ src/Components/rest/HouseApi.js
@@ -0,0 +1,34 @@
+const HOUSES_ENDPOINT = 'https://ancient-taiga-31359.herokuapp.com/api/houses';
+
+class HouseApi {
+
+    
+    get = async () => {
+
+     try{
+        const resp = await fetch(HOUSES_ENDPOINT);
+        const data = await resp.json();
+        return data;
+    } catch(e) {
+        console.log('Oops,', e);
+    }
+    }
+
+    put = async (house) => {
+        try{
+        const resp = await fetch (`${HOUSES_ENDPOINT}/${house._id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/jason'
+            },
+            body: JSON.stringify(house)
+        });
+        return await resp.json();
+    } catch (e) {
+        console.log('Oops', e);
+
+    }
+    }
+}
+
+export const houseApi = new HouseApi ();
